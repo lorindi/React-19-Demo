bf4c779ae7957e4be4650000b53f11216dd12e49
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-29 17:15:34 +0300

    Create UseOptimisticPresVersion.jsx

---
+++ src/components/useOptimistic/UseOptimisticPresVersion.jsx
@@ -0,0 +1,53 @@
+import { useActionState, useState } from "react";
+// import uniqid from "uniqid";
+
+const createUniqueId = () => '_' + Math.random().toString(36).substr(2, 9);
+
+const createTodo = (title) =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve({ id: createUniqueId(), title: "" }))
+  );
+
+function UseOptimisticPresVersion() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Clean" },
+    { id: 2, title: "Study" },
+  ]);
+
+  const [error, formAction, isPending] = useActionState(
+    async (previousState, formData) => {
+      const title = formData.get("name");
+
+      if (!title) {
+        return "Title ir required";
+      }
+
+      const newTodo = await createTodo(title);
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+      return null;
+    },
+    null
+  );
+
+  return (
+    <div>
+      <h2>UseOptimisticPresVersion</h2>
+      <form action={formAction}>
+        <input type="text" name="name" />
+        <button type="submit" disabled={isPending}>
+          Add
+        </button>
+      </form>
+      {error && <p>{error}</p>}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} style={todo.pending && { color: "gray" }}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UseOptimisticPresVersion;
