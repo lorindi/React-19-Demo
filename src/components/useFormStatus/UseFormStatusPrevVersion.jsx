372bf68a7e4bcdd5d484e1fefe0f3e2849432f3c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-29 17:15:29 +0300

    Create UseFormStatusPrevVersion.jsx

---
+++ src/components/useFormStatus/UseFormStatusPrevVersion.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const updateName = (name) =>
+  new Promise((resolve) => setTimeout(() => resolve(name), 2000));
+
+function Submit({ pending }) {
+  return (
+    <button type="submit" disabled={pending}>
+      Update
+    </button>
+  );
+}
+function UseFormStatusPrevVersion() {
+  const [pending, setPending] = useState(false);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    setPending(true);
+    const result = await updateName(formData.get("name"));
+    setPending(false);
+    
+    console.log(result);
+    e.target.reset();
+  };
+  return (
+    <>
+      <h2>Use Form Status Prev Version</h2>
+      <form onSubmit={submitHandler}>
+        <input type="text" name="name" />
+        <Submit pending={pending} />
+      </form>
+    </>
+  );
+}
+
+export default UseFormStatusPrevVersion;
