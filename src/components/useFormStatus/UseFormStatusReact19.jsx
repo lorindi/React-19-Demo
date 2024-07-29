86a9f642b6d049a9b11c3e11593ded112066574c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-29 17:15:31 +0300

    Create UseFormStatusReact19.jsx

---
+++ src/components/useFormStatus/UseFormStatusReact19.jsx
@@ -0,0 +1,33 @@
+import { useFormStatus } from "react-dom";
+
+const updateName = (name) =>
+  new Promise((resolve) => setTimeout(() => resolve(name), 2000));
+
+function Submit() {
+  const {pending, data, method, action} = useFormStatus();
+  return (
+    <button type="submit" disabled={pending}>
+      Update
+    </button>
+  );
+}
+
+function UseFormStatusReact19() {
+
+  const formAction = async (formData) => {
+    const result = await updateName(formData.get("name"));
+    
+    console.log(result);
+  };
+  return (
+    <>
+      <h2>Use Form Status React 19</h2>
+      <form action={formAction}>
+        <input type="text" name="name" />
+        <Submit />
+      </form>
+    </>
+  );
+}
+
+export default UseFormStatusReact19;
