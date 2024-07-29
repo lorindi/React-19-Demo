3200ddd8c0aeb27247dbf06ce9fb0c43ea1729ed
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-29 18:09:14 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
 import FormPrevVersion from "./components/uncontrolledForm/FormPrevVersion";
 import FormReact19 from "./components/uncontrolledForm/FormReact19";
 
-import UseActionStatePrevVersion from "./components/useActionState/UseActionStatePrevVersion";
-import UseActionStateReact19 from "./components/useActionState/UseActionStateReact19";
-
 import UseFormStatusPrevVersion from "./components/useFormStatus/UseFormStatusPrevVersion";
 import UseFormStatusReact19 from "./components/useFormStatus/UseFormStatusReact19";
-
-import UseOptimisticPresVersion from "./components/useOptimistic/useOptimisticPresVersion";
-import UseOptimisticReact19 from "./components/useOptimistic/useOptimisticReact19";
 
 import UseTransitionPrevVersion from "./components/useTransition/UseTransitionPrevVersion";
 import UseTransitionReact19 from "./components/useTransition/UseTransitionReact19";
 
+import UseActionStatePrevVersion from "./components/useActionState/UseActionStatePrevVersion";
+import UseActionStateReact19 from "./components/useActionState/UseActionStateReact19";
+
+
+import UseOptimisticPrevVersion from "./components/useOptimistic/useOptimisticPresVersion";
+import UseOptimisticReact19 from "./components/useOptimistic/useOptimisticReact19";
+
+import './App.css'
 
 function App() {
   return (
-    <div className="">
-      <div>
+    <div className="wrapper">
+      <div className="containerDemo">
         <FormPrevVersion />
         <FormReact19 />
       </div>
-      <div>
+      <div className="containerDemo">
         <UseFormStatusPrevVersion />
         <UseFormStatusReact19 />
       </div>
-      <div>
+      <div className="containerDemo">
         <UseTransitionPrevVersion />
         <UseTransitionReact19 />
       </div>
-      <div>
+      <div className="containerDemo">
         <UseActionStatePrevVersion />
         <UseActionStateReact19 />
       </div>
-      <div>
-        <UseOptimisticPresVersion />
+      <div className="containerDemo">
+        <UseOptimisticPrevVersion />
         <UseOptimisticReact19 />
       </div>
     </div>
